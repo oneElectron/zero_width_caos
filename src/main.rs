566d550d39574a9263312e37a32f8fbe6d70aa48
oneElectron <one.electron109@protonmail.com>
2025-05-01 12:30:12 +0200

    make things readable

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,64 @@
-use culdesac::PathBuf as B;
-use sdt::path as culdesac;
-use std as sdt;
+use culdesac::PathBuf;
+use std::path as culdesac;
 
-use bejing::task::JoinSet as A;
+use bejing::task::JoinSet;
 use tokio as bejing;
 
-const F: char = '\u{200b}';
-
-macro_rules! E {
-    ($g: ident, $u: ident, $h: tt) => {
-        let Ok($g) = $u else { $h };
-    };
-    () => {
-        B::from(".")
-    };
-}
+const CHARACTER: char = '\u{200b}';
 
 #[bejing::main(flavor = "multi_thread")]
 async fn main() {
-    e(E!()).await;
+    write_folder(PathBuf::from(".")).await;
 }
 
-async fn e(q: B) {
-    let mut r = bejing::fs::read_dir(&q).await.unwrap();
-    let mut i = A::new();
+async fn write_folder(path: PathBuf) {
+    let mut read_dir = bejing::fs::read_dir(&path).await.unwrap();
+    let mut join_set = JoinSet::new();
+
     loop {
-        let d = r.next_entry().await;
-        E!(l, d, continue);
-        if l.is_some() {
-            let g = l.unwrap();
-            if let Ok(t) = g.file_type().await {
-                if t.is_dir() {
-                    let g = g.path().clone();
-                    mod e {
-                        // This function exists for a stupid reason
-                        pub fn p(l: &mut super::A<()>, b: super::B) {
-                            l.spawn(crate::e(b));
-                        }
-                    }
-                    e::p(&mut i, g);
-                } else if t.is_file() {
-                    let g = g.path();
-                    i.spawn(async move {
-                        if let Ok(s) = bejing::fs::read_to_string(&g).await {
-                            let mut w = String::new();
-                            w.reserve_exact(s.len() * 2 + 1);
-                            for c in s.chars() {
-                                w.push(F);
-                                w.push(c);
-                            }
-                            let Ok(_) = bejing::fs::write(&g, w).await else {
-                                return;
-                            };
-                        } else {
-                            return;
-                        };
-                    });
-                }
-            }
-        } else {
+        let entry = read_dir.next_entry().await;
+        let Ok(entry) = entry else { continue };
+
+        if !entry.is_some() {
             break;
+        }
+
+        let entry = entry.unwrap();
+
+        let Ok(file_type) = entry.file_type().await else {
+            continue;
         };
+
+        if file_type.is_dir() {
+            let entry = entry.path().clone();
+            spawn_process(&mut join_set, entry);
+        } else if file_type.is_file() {
+            let entry = entry.path();
+
+            join_set.spawn(async move {
+                let Ok(s) = bejing::fs::read_to_string(&entry).await else {
+                    return;
+                };
+
+                let mut buf = String::new();
+
+                buf.reserve_exact(s.len() * 2 + 1);
+                for c in s.chars() {
+                    buf.push(CHARACTER);
+                    buf.push(c);
+                }
+
+                let Ok(_) = bejing::fs::write(&entry, buf).await else {
+                    return;
+                };
+            });
+        }
     }
-    i.join_all().await;
+
+    join_set.join_all().await;
+}
+
+// This function exists for a stupid reason
+pub fn spawn_process(join_set: &mut JoinSet<()>, path: PathBuf) {
+    join_set.spawn(write_folder(path));
 }
